ab26cc4402767650b57b5af709966c377446214a
damndanial <68948292+damndanial@users.noreply.github.com>
2020-08-04 11:18:04 +0500

    Made a very simple player movement script, I suggest using it

---
+++ Brackey's 2020 Game Jam/Assets/Scripts/Dan/SimplePlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask GroundLayer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, GroundLayer);
+
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+        if (isGrounded && Input.GetButton("Jump"))
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+    }
+}
