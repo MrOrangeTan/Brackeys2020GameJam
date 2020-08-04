0ccf8d219e43519c513781d5fe0358d5d6af5f11
ThatBoii52 <goiancoop8@gmail.com>
2020-08-03 22:16:17 -0700

    Jump
    You can jump

--- Brackey's 2020 Game Jam/Assets/Scripts/warrior/PlayerMovement.cs
+++ Brackey's 2020 Game Jam/Assets/Scripts/warrior/PlayerMovement.cs
@@ -8,15 +8,22 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D Controller;
     float HorizontalMove = 0f;
     public float Speed = 10f;
+    bool jump = false;
 
 
     void Update ()
     {
         HorizontalMove = Input.GetAxisRaw("Horizontal") * Speed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     void FixedUpdate ()
     {
-        Controller.Move(HorizontalMove * Time.fixedDeltaTime, false, false);
+        Controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
