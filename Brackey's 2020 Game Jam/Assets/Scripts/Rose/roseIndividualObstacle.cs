fc2ff80f3fffda96fdde153bedf73ab6b4e42ae2
RoseChanZeroTwo <69076166+RoseChanZeroTwo@users.noreply.github.com>
2020-08-04 16:19:37 +0800

    pull origin

---
+++ Brackey's 2020 Game Jam/Assets/Scripts/Rose/roseIndividualObstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roseIndividualObstacle : MonoBehaviour
+{
+    //duration before they fall
+    [SerializeField] private float maxTimeBeforeBreak;
+    private float currentTimeBeforeBreak;
+
+    //how long will it take for the blocks to respawn
+    [SerializeField] private float maxRespawnTime;
+    private float currentRespawnTime;
+
+    private void Awake()
+    {
+        currentTimeBeforeBreak = maxTimeBeforeBreak;
+        currentRespawnTime = maxRespawnTime;
+    }
+
+}
