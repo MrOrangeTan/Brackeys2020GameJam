9ebadd3da93e32cf3161878eb1def4c362ceb226
RoseChanZeroTwo <69076166+RoseChanZeroTwo@users.noreply.github.com>
2020-08-07 00:09:03 +0800

    up and down moving platforms

---
+++ Brackey's 2020 Game Jam/Assets/Scripts/Rose/Obstacles/upAndDownPlatorm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class upAndDownPlatorm : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float minDistance;
+    [SerializeField] private float maxDistance;
+
+    bool movingUp;
+
+    private void Update()
+    {
+     if (transform.position.y > maxDistance)
+        {
+            movingUp = false;
+        }
+     if (transform.position.y < minDistance)
+        {
+            movingUp = true;
+        }
+
+     if (movingUp)
+        {
+            transform.Translate(new Vector2(0, movementSpeed)* Time.deltaTime);
+        }
+     else
+        {
+            transform.Translate(new Vector2(0, -movementSpeed) * Time.deltaTime);
+        }
+    }
+}
