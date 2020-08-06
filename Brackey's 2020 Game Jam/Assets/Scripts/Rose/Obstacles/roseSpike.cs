338240743c2550a376ccbebb2a4ed042840ffeb0
RoseChanZeroTwo <69076166+RoseChanZeroTwo@users.noreply.github.com>
2020-08-06 13:12:33 +0800

    Added interaction with spike
    Text only

--- Brackey's 2020 Game Jam/Assets/Scripts/Rose/Obstacles/roseSpike.cs
+++ Brackey's 2020 Game Jam/Assets/Scripts/Rose/Obstacles/roseSpike.cs
@@ -7,16 +7,26 @@ public class roseSpike : MonoBehaviour
     [SerializeField] private float damageDone;
 
     [SerializeField] private playerClass classForPlayer;
+    [HideInInspector] public bool spikeHit;
+
+    //setting the variable for the script
+    public healthInteractions healthUpdate;
 
     private void Awake()
     {
+        //getting the component
+        healthUpdate = FindObjectOfType<healthInteractions>();
+        spikeHit = false;
         classForPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<playerClass>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            classForPlayer.health -= damageDone;
+            classForPlayer.currentHealth -= damageDone;
+            //if it hits the player, the component will be equilavent to this spike
+            healthUpdate.spike = this;
+            spikeHit = true;
         }
     }
 }
---
+++ Brackey's 2020 Game Jam/Assets/Scripts/Rose/UI/healthInteractions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthInteractions : MonoBehaviour
+{
+    public Text healthText;
+    public roseSpike spike;
+    [SerializeField] private playerClass classForPlayer;
+
+    private void Awake()
+    {
+        
+        healthText.text = "Health: " + classForPlayer.health;
+    }
+
+    private void Update()
+    {
+        if (spike.spikeHit)
+        {
+            healthText.text = "Health: " + classForPlayer.currentHealth;
+        }
+    }
+}
