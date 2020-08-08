aec8dafab4d30848f520de4d0fae69477a002690
damndanial <68948292+damndanial@users.noreply.github.com>
2020-08-08 11:22:43 +0500

    Menu changes

---
+++ Brackey's 2020 Game Jam/Assets/Scripts/Static.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Static : MonoBehaviour {
+
+    public Texture2D[] frames;
+    public int fps = 60;
+
+    private void Start()
+    {
+
+        GetComponent<RawImage>().texture = frames[0];
+
+    }
+
+    private void Update()
+    {
+
+        int index = (int)(Time.time * fps) % frames.Length;
+        GetComponent<RawImage>().texture = frames[index];
+
+    }
+
+}
