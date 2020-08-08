e63b48aab7662e51b54b30b012e4b64ba16ab984
RoseChanZeroTwo <69076166+RoseChanZeroTwo@users.noreply.github.com>
2020-08-08 14:31:29 +0800

    cameraFollow script

---
+++ Brackey's 2020 Game Jam/Assets/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime = 0.2f;
+    private Vector3 velocity;
+    [SerializeField] private Vector3 changeTargetPosition = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPosition = target.TransformPoint(changeTargetPosition);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
