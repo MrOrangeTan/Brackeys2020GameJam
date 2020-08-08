9e43193952a38fc877d18ba8e811d487335c7b4d
damndanial <68948292+damndanial@users.noreply.github.com>
2020-08-08 13:54:08 +0500

    sdgdbfsv

--- Brackey's 2020 Game Jam/Assets/EnemyAI.cs
+++ Brackey's 2020 Game Jam/Assets/EnemyAI.cs
@@ -10,14 +10,36 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
-        if (speed < 1)
-            speed += 0.05f;
         if (waypoints.Length == 2)
+        {
+            if (speed < 1)
+                speed += 0.05f;
             transform.position = Vector3.Lerp(waypoints[0].position, waypoints[1].position, speed - rewindFactor);
+            if (PlayerMovement.rewind && rewindFactor < 1)
+                rewindFactor += 0.01f;
+            else if (rewindFactor > 0)
+                rewindFactor -= 0.01f;
+        }
 
-        if (PlayerMovement.rewind && rewindFactor < 1)
-            rewindFactor += 0.01f;
-        else if (rewindFactor > 0)
-            rewindFactor -= 0.01f;
+        if (waypoints.Length == 3)
+        {
+            if (speed < 2)
+                speed += 0.05f;
+
+            if (speed - rewindFactor > 0 && speed - rewindFactor <= 1)
+            {
+                transform.position = Vector3.Lerp(waypoints[0].position, waypoints[1].position, speed - rewindFactor);
+                transform.eulerAngles = new Vector3(0, 0, -90);
+            }
+            else if (speed - rewindFactor > 1)
+            {
+                transform.position = Vector3.Lerp(waypoints[1].position, waypoints[2].position, speed - rewindFactor - 1);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            if (PlayerMovement.rewind && rewindFactor < 2)
+                rewindFactor += 0.01f;
+            else if (rewindFactor > 0)
+                rewindFactor -= 0.01f;
+        }
     }
 }
--- Brackey's 2020 Game Jam/Assets/Scripts/Game Scripts/GameStart.cs
+++ Brackey's 2020 Game Jam/Assets/Scripts/Game Scripts/GameStart.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    public GameObject Song;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Room1");
+    }
+
+    private void Start()
+    {
+        try
+        {
+            if (FindObjectOfType<AudioSource>())
+                return;
+            GameObject s = Instantiate(Song);
+            DontDestroyOnLoad(s);
+        }
+        catch { }
     }
 }
