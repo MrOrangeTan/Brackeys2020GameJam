7f4c0034aa0c42589d908675e4f5bb630a96b5cc
RoseChanZeroTwo <69076166+RoseChanZeroTwo@users.noreply.github.com>
2020-08-08 15:43:28 +0800

    pull

--- Brackey's 2020 Game Jam/Assets/cameraFollow.cs
+++ Brackey's 2020 Game Jam/Assets/cameraFollow.cs
@@ -6,13 +6,13 @@ public class cameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private float smoothTime = 0.2f;
-    private Vector3 velocity;
-    [SerializeField] private Vector3 changeTargetPosition = Vector3.zero;
+    private Vector3 velocity= Vector3.zero;
+    [SerializeField] private Vector3 changeTargetPosition;
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetPosition = target.TransformPoint(changeTargetPosition);
+        Vector3 targetPosition = target.TransformPoint(changeTargetPosition);
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
